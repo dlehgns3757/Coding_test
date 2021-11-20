1cf716d5325a14d27ced5f79470265f76c269ec9
Dohoon Lee <92811035+dlehgns3757@users.noreply.github.com>
2021-11-20 17:42:51 +0900

    Update Brute_force_2(소수찾기).py

--- Coding_test/5. Brute_force/Brute_force_2(소수찾기).py
+++ Coding_test/5. Brute_force/Brute_force_2(소수찾기).py
@@ -1,6 +1,7 @@
 import itertools
 import re
 
+# 소수 구하는 함수
 def isPrime(a):
     if(a<2):
         return False
@@ -19,10 +20,10 @@ def solution(numbers):
         num.append(int(n))
         
     for i in range(len(num)):
-        for j in list(itertools.permutations(num, i+1)):
+        for j in list(itertools.permutations(num, i+1)): # permutations로 모든 순열 생성
             j = str(j)
             characters = ")(, "
-            j = ''.join( x for x in j if x not in characters)
+            j = ''.join( x for x in j if x not in characters) # permutations는 튜플을 반환하기 때문에 문자열로 변환 후 불필요한 문자 제거 
             all_com.append(re.sub(", ","",str(j)))
     
     for a in all_com:
@@ -36,3 +37,8 @@ def solution(numbers):
             cnt += 1
     
     return cnt
+
+if __name__ == '__main__':
+    numbers = "17"
+    
+    solution(numbers)
